f5fd6a498ffd42b5eb86f3553b9730f94a10784e
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2021-01-06 07:09:32 +0200

    Create Home_test.js

---
+++ Pages/Home_test.js
@@ -0,0 +1,23 @@
+document.getElementById("bookingpage").onclick = function(){
+    if (condition) {
+        window.open("bookingform.html","_self");
+    } else {
+        alert("Please Log in First.");
+    };
+}
+
+document.getElementById("profilepage").onclick = function(){
+    if (condition) {
+        window.open("profile.html","_self");
+    } else {
+        alert("Please Log in First.");
+    };
+}
+
+document.getElementById("btn.sign").onclick = function(){
+    if (condition) {
+        window.open("Form.html","_self");
+    } else {
+        alert("Please Log in First.");
+    };
+}
