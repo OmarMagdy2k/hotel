ec8ecf474a22a35fe9247bb5d213d5183fc791fa
khaloud21k <kholoudelbaroudy19@gmail.com>
2021-01-08 00:01:50 +0200

    Create profile.php

---
+++ Pages/profile.php
@@ -0,0 +1,33 @@
+//connect database
+<?php
+$host="127.0.0.1:3322";
+$user="root";
+$password="";
+$database="online_hotel_reservation";
+$connect=mysqli_connect($host,$user,$password,$database);
+?>
+<?php
+$res_id=$_POST['Res_ID'];
+//read from data 
+$query= select email,first_name,last_name,SSN, password,card_number,birthday
+from  user join reservation
+on  user.user_id=reservation.user_id
+WHERE res_Id=.$res_id;
+$result=mysqli_query($connect,$query);
+?>
+  <?php
+  //write from database
+while ($row=mysqli_fetch_assoc($result))
+{
+    $email=.$row['email'];
+    $FN=.$row['first_name'];
+    $LN=.$row['last_name'];
+    $SNN=.$row['SSN'];
+   $password =.$row['password'];
+   $cardnum =.$row['card_number'];
+   $birth=.$row['birthday'];
+    ?>
+//close connection
+<?php
+mysqli_close($connect);
+?>
