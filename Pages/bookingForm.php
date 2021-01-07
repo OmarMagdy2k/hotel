f155346a6031b2236eb12a28118eeb87071079b2
khaloud21k <kholoudelbaroudy19@gmail.com>
2021-01-08 00:01:29 +0200

    Update bookingForm.php

--- Pages/bookingForm.php
+++ Pages/bookingForm.php
@@ -9,6 +9,8 @@ $connect=mysqli_connect($host,$user,$password,$database);
 <?php
 if(isset($_POST['done']))
 {
+    $roomtype=$_POST['roomtype'];
+    $userid=$_POST['userid'];
     $cash=$_POST['cash'];
     $credit=$_POST['credit'];
     $total_price=$_POST['tp'];
@@ -22,9 +24,13 @@ if(isset($_POST['done']))
     }
     $card_num=$_POST['card'];
     $exp_date=$_POST['exdate'];
-    $sql="insert into reservation( no_of_guests , check_in , check_out , payment)values('". $noofguests."','".$check_in."','".$check_out."','".$payment_way."') ";
-    $sql1="insert into user(card_number ,exp_date) vaues('".$card_num."','".$exp_date."')";
+    $sql="insert into reservation( no_of_guests , check_in , check_out , payment,total_price)values('". $noofguests."','".$check_in."','".$check_out."','".$payment_way."','" $total_price"') 
+    WHERE user_id=.$userid and type=.$roomtype";
+    $sql1="insert into user(card_number ,exp_date) vaues('".$card_num."','".$exp_date."')
+   WHERE user_id=.$userid";
     $result1=  mysqli_query($connect, $sql,$sql1);
+    //select reservation ID
+    $Res_ID=select res_Id  from reservation WHERE user_id=.$userid;
     ?>
     //close connection
 <?php
