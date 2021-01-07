4c1a7be59d99f5b1bb9a4eb4a46d6682138e4532
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2021-01-07 13:33:47 +0200

    Create bookingForm.php

---
+++ Pages/bookingForm.php
@@ -0,0 +1,32 @@
+//connect database
+<?php
+$host="127.0.0.1:3322";
+$user="root";
+$password="";
+$database="online_hotel_reservation";
+$connect=mysqli_connect($host,$user,$password,$database);
+?>
+<?php
+if(isset($_POST['done']))
+{
+    $cash=$_POST['cash'];
+    $credit=$_POST['credit'];
+    $total_price=$_POST['tp'];
+    $check_in=$_POST['cin'];
+    $check_out=$_POST['cout'];
+    $noofguests=$_POST['numofguests'];
+    if empty ($cash) {
+        $payment_way=$_POST['credit'];
+    } else {
+        $payment_way=$_POST['cash'];
+    }
+    $card_num=$_POST['card'];
+    $exp_date=$_POST['exdate'];
+    $sql="insert into reservation( no_of_guests , check_in , check_out , payment)values('". $noofguests."','".$check_in."','".$check_out."','".$payment_way."') ";
+    $sql1="insert into user(card_number ,exp_date) vaues('".$card_num."','".$exp_date."')";
+    $result1=  mysqli_query($connect, $sql,$sql1);
+    ?>
+    //close connection
+<?php
+mysqli_close($connect);
+?>
