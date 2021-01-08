ebf583f4b3abaa2c92e14e19f28333a97f3f75fe
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2021-01-08 08:30:25 +0200

    Update summary.php

--- Pages/summary.php
+++ Pages/summary.php
@@ -5,29 +5,23 @@ $user="root";
 $password="";
 $database="online_hotel_reservation";
 $connect=mysqli_connect($host,$user,$password,$database);
-?>
-<?php
+
 $res_id=$_POST['Res_ID'];
 //read from data 
-$query= select room_number,floor_number,res_Id, check_in,check_out,payment,total_price
+$query= "SELECT room_number,floor_number,res_Id, check_in,check_out,payment,total_price
 from  rooms join reservation
 on  rooms.room_ID=reservation.room_ID
-WHERE res_Id=.$res_id;
-$result=mysqli_query($connect,$query);
-?>
-<?php
+WHERE res_Id=.$res_id";
+$result= mysqli_query($connect,$query);
 //write from database
-while ($row=mysqli_fetch_assoc($result))
-{
-  $email=.$row['room_number'];
-  $FN=.$row['floor_number'];
-  $LN=.$row['res_Id'];
-  $SNN=.$row['check_in'];
- $password =.$row['check_out'];
- $cardnum =.$row['payment'];
- $birth=.$row['total_price'];
-  ?>
+while ($row=mysqli_fetch_assoc($result)){
+  $email=$row['room_number'];
+  $FN=$row['floor_number'];
+  $LN=$row['res_Id'];
+  $SNN=$row['check_in'];
+  $password =$row['check_out'];
+  $cardnum =$row['payment'];
+  $birth=$row['total_price'];
+}
   //close connection
-  <?php
 mysqli_close($connect);
-?>
