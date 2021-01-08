98065ecdf1306385686259ab006c8ef089b3a1bc
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2021-01-08 08:30:28 +0200

    Update Summary_test.js

--- Pages/Summary_test.js
+++ Pages/Summary_test.js
@@ -1,4 +1,34 @@
-//var Res_ID = ;
+var Res_ID ="";
+
+
+document.getElementById("homepage").onclick = function () {
+    window.open("home.html", "_self");
+}
+document.getElementById("bookingpage").onclick = function () {
+    if (userId !== "") {
+        window.open("bookingform.html", "_self");
+    } else {
+        alert("Please Log in First.");
+    };
+}
+
+document.getElementById("profilepage").onclick = function () {
+    if (userId !== "") {
+        window.open("profile.html", "_self");
+    } else {
+        alert("Please Log in First.");
+    };
+}
+
+document.getElementById("roomspage").onclick = function () {
+    window.open("Rooms.html", "_self");
+}
+
+document.getElementById("reservationpage").onclick = function () {
+    window.open("Reservation.html", "_self");
+}
+
+
 var getSummary= new XMLHttpRequest();
 
 getSummary.onreadystatechange = function (){
@@ -12,6 +42,21 @@ getSummary.onreadystatechange = function (){
 getSummary.open('GET','getSummary.php',true);
 getSummary.send(Res_ID);
 
+function adminPart() {
+    if (userId === "1") {
+        document.getElementById("roomspage").style.display = "block";
+        document.getElementById("reservationpage").style.display = "block";
+    } else {
+        document.getElementById("reservationpage").style.display = "none";
+        document.getElementById("roomspage").style.display = "none";
+    }
+    document.getElementById("roomspage").style.display = "none";
+    document.getElementById("reservationpage").style.display = "none";
+}
+
+window.onload = function(){
+    adminPart();
+}
 /* php json
 {
     roomNum: 1,
