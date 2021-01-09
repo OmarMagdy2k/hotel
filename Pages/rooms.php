9b4a02387e91a60a57816c8dc97c694883eafe49
khaloud21k <kholoudelbaroudy19@gmail.com>
2021-01-09 07:32:40 +0200

    Update rooms.php

--- Pages/rooms.php
+++ Pages/rooms.php
@@ -4,24 +4,18 @@ $user="root";
 $password="";
 $database="online_hotel_reservation";
 $connect=mysqli_connect($host,$user,$password,$database);
-?>
-<?php
+
         //read from data 
-        $query= select room_number,type,floor_number
+        $query=" select room_number,type,floor_number
         from roomtype join rooms   
-        on ( roomtype.room_type_ID=rooms.room_type_ID);
+        on ( roomtype.room_type_ID=rooms.room_type_ID)";
         $result=mysqli_query($connect,$query);
-        ?>
-          <?php
-          //write from database
-        while ($row=mysqli_fetch_assoc($result))
-        {
-          echo  "<tr><td>".$row[' room_number']. "</td><td>".$row['type']."</td><td>".$row['floor_number']."</td><td>" 
-         <a href="delete1.php?floor_number='.$row['floor_number'].'">Delete</a> "</td><td>" <a href="edit.php?floor_number='.$row['floor_number'].'">Edit</a>  "</td></tr>";
-                ?>
-                <?php 
-                mysqli_free_result($result);
-                ?>
-                <?php
+        //write from database
+while ($row = mysqli_fetch_assoc($result)) {
+  $room_number = $row['room_number'];
+  $roomtype = $row['type'];
+  $floor_number = $row['floor_number'];
+}
+echo json_encode($row);
+//close connection
 mysqli_close($connect);
-?>
