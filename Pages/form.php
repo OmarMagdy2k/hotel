0e869f030e32c185ab1b5f35f7837cfbaf5ef958
OmarMagdy2k <74633590+OmarMagdy2k@users.noreply.github.com>
2021-01-09 06:09:59 +0200

    Update form.php

--- Pages/form.php
+++ Pages/form.php
@@ -1,34 +1,36 @@
-//connect database
 <?php
-$host="127.0.0.1:3322";
-$user="root";
-$password="";
-$database="online_hotel_reservation";
-$connect=mysqli_connect($host,$user,$password,$database);
-?>
-<?php
-    if(isset($_POST['Sign Up']))
-{
-$first_name=$_POST['First Name'];
-    $Last_Name=$_POST['Last Name'];  
-    $Phone_Number=$_POST['Phone Number']; 
-    $Birthday=$_POST['Birthday']; 
-    $SNN=$_POST['SNN']; 
-    $email=$_POST['email']; 
-    $address=$_POST['address'];
-    $psw=$_POST['psw']; 
-    $sql = "insert into user ( first_name ,last_name ,email,SSN  ,mobile_number,address ,birthday,password )
-    values('".$first_name."','". $Last_Name."','".$email."','".$SNN."','". $Phone_Number."','".$address."','". $Birthday."','". $psw."')";
-    $result=  mysqli_query($connect, $sql);
-    if( $result){
-        echo '</br>Data is inserted';
-    } 
-else {
-die("Database query failed. " . mysqli_error($connection));
-}// select user id
-$userid="SELECT user_id FROM user WHERE email='$email'";
-}?>
+$host = "127.0.0.1:3322";
+$user = "root";
+$password = "";
+$database = "online_hotel_reservation";
+$connect = mysqli_connect($host, $user, $password, $database);
+
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
+
+$first_name = $obj->firstname;
+$last_name = $obj->lastname;
+$mobile_number = $obj->phonenumber;
+$birthday = $obj->birthday;
+$SSN = $obj->SSN;
+$email = $obj->email;
+$address = $obj->address;
+$pass = $obj->pass;
+$sql = "insert into user ( first_name ,last_name ,email,SSN  ,mobile_number,address ,birthday,password )
+    values('" . $first_name . "','" . $last_name . "','" . $email . "','" . $SSN . "','" . $mobile_number . "','" . $address . "','" . $birthday . "','" . $pass . "')";
+$result =  mysqli_query($connect, $sql);
+
+session_start();
+// select user
+$sql2 = "SELECT * FROM user WHERE email='$email' and password='$pass'";
+$result2 = mysqli_query($connect, $sql2);
+if (($row2 = mysqli_fetch_assoc($result2))) {
+    $encodedUser = json_encode($row2);
+    $_SESSION['user'] = $encodedUser;
+    echo '1';
+} else {
+    echo "Error: Incorrect username or password.";
+}
+
 //close connection
-<?php
 mysqli_close($connect);
-?>
